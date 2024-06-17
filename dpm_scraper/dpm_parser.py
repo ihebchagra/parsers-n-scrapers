ee4f474685562834f3e2e72d008d89206377d666
Iheb Chagra <ihebchagra@gmail.com>
2024-06-17 22:27:34 +0100

    mklfjqfslk

--- dpm_scraper/dpm_parser.py
+++ dpm_scraper/dpm_parser.py
@@ -8,8 +8,9 @@ import json
 autocomplete = []
 json_output=[]
 
-for h in [['dpm.html','pages_downloaded'],['dpmsup.html','pages_downloaded_sup'],['dpmsus.html','pages_downloaded_sus']]:
-	html_doc=open(h[0])
+#for h in [['dpm.html','pages_downloaded'],['dpmsup.html','pages_downloaded_sup']]:
+for h in [['dpm.html','pages_downloaded']]:
+	html_doc=open(h[0],encoding='utf-8')
 	print("loading " + h[0] + " file")
 	bigsoup=BeautifulSoup(html_doc, 'html.parser')
 	html_doc.close()
@@ -22,7 +23,7 @@ for h in [['dpm.html','pages_downloaded'],['dpmsup.html','pages_downloaded_sup']
 		f = os.path.join(directory, filename)
 		if not os.path.isfile(f):
 			continue
-		fiche = open(f)
+		fiche = open(f,encoding='utf-8')
 		soup = BeautifulSoup(fiche, 'html.parser')
 		fiche.close()
 		object = {}
@@ -37,21 +38,24 @@ for h in [['dpm.html','pages_downloaded'],['dpmsup.html','pages_downloaded_sup']
 				if valeur not in autocomplete:
 					autocomplete.append(valeur)
 			object.update({index : valeur})
-		bigparent=bigsoup.find(string=re.compile(object['AMM'])).parent.parent
-		if bigparent.find('a',href=re.compile("rcp.pdf")) is not None:
-			valeur=bigparent.find('a',href=re.compile("rcp.pdf"))['href'].replace('../..','http://www.dpm.tn')
-			index='rcp'
-			object.update({index : valeur})
-		if bigparent.find('a',href=re.compile("notice.pdf")) is not None:
-			valeur=bigparent.find('a',href=re.compile("notice.pdf"))['href'].replace('../..','http://www.dpm.tn')
-			index='notice'
-			object.update({index : valeur})
+		try:
+			bigparent=bigsoup.find(string=re.compile(object['AMM'])).parent.parent
+			if bigparent.find('a',href=re.compile("rcp.pdf")) is not None:
+				valeur=bigparent.find('a',href=re.compile("rcp.pdf"))['href'].replace('../..','http://www.dpm.tn')
+				index='rcp'
+				object.update({index : valeur})
+			if bigparent.find('a',href=re.compile("notice.pdf")) is not None:
+				valeur=bigparent.find('a',href=re.compile("notice.pdf"))['href'].replace('../..','http://www.dpm.tn')
+				index='notice'
+				object.update({index : valeur})
+		except:
+			print("no AMM")
 		json_output.append(object)
 
 print("writing to medicaments.js")
 json_final = json.dumps(json_output, sort_keys=False,ensure_ascii=False)
-open("medicaments.js","w").write("data="+json_final)
+open("medicaments.js","w",encoding='utf-8').write("data="+json_final)
 
 print("writing to autocomplete.js")
 json_final = json.dumps(autocomplete, sort_keys=False,ensure_ascii=False)
-open("autocomplete.js","w").write("autocomplete_list="+json_final)
+open("autocomplete.js","w",encoding='utf-8').write("autocomplete_list="+json_final)
--- dpm_scraper/dpm_scraper.py
+++ dpm_scraper/dpm_scraper.py
@@ -21,44 +21,26 @@ except:
 	print("sus already created")
 
 
-
-
-print('downloading suspendus')
-html_doc=open("dpmsus.html")
-soup = BeautifulSoup(html_doc, 'html.parser')
-num_medic=len(soup.find_all('a',href=re.compile('fiche\.php')))
-i=0
-for link in soup.find_all('a',href=re.compile('fiche\.php')):
-	i=i+1
-	cod_medic=link['href'].replace('fiche.php?cod_medic=','')
-	print('getting', i, '/', num_medic)
-	url=f'http://www.dpm.tn/dpm_pharm/medicament/fiche.php?cod_medic={cod_medic}'
-	r = requests.get(url, allow_redirects=True)
-	open(f"pages_downloaded_sus/{cod_medic}.html","wb").write(r.content)
-
-
-print('downloading supprimés')
-html_doc=open("dpmsup.html")
-soup = BeautifulSoup(html_doc, 'html.parser')
-num_medic=len(soup.find_all('a',href=re.compile('fiche2\.php')))
-i=0
-for link in soup.find_all('a',href=re.compile('fiche2\.php')):
-	i=i+1
-	cod_medic=link['href'].replace('fiche2.php?cod_medic=','')
-	print('getting', i, '/', num_medic)
-	url=f'http://www.dpm.tn/dpm_pharm/medicament/fiche2.php?cod_medic={cod_medic}'
-	r = requests.get(url, allow_redirects=True)
-	open(f"pages_downloaded_sup/{cod_medic}.html","wb").write(r.content)
-
-
+#print('downloading suspendus')
+#html_doc=open("dpmsus.html", 'r',encoding='utf-8')
+#soup = BeautifulSoup(html_doc, 'html.parser')
+#num_medic=len(soup.find_all('a',href=re.compile('fiche.php')))
+#i=0
+#for link in soup.find_all('a',href=re.compile('fiche.php')):
+#	i=i+1
+#	cod_medic=link['href'].replace('fiche.php?cod_medic=','')
+#	print('getting', i, '/', num_medic)
+#	url=f'http://www.dpm.tn/dpm_pharm/medicament/fiche.php?cod_medic={cod_medic}'
+#	r = requests.get(url, allow_redirects=True)
+#	open(f"pages_downloaded_sus/{cod_medic}.html","wb").write(r.content)
 
 
 print('downloading spécialité')
-html_doc=open("dpm.html")
+html_doc=open("dpm.html","r",encoding='utf-8')
 soup = BeautifulSoup(html_doc, 'html.parser')
-num_medic=len(soup.find_all('a',href=re.compile('fiche\.php')))
+num_medic=len(soup.find_all('a',href=re.compile('fiche.php')))
 i=0
-for link in soup.find_all('a',href=re.compile('fiche\.php')):
+for link in soup.find_all('a',href=re.compile('fiche.php')):
 	i=i+1
 	cod_medic=link['href'].replace('fiche.php?cod_medic=','')
 	print('getting', i, '/', num_medic)
